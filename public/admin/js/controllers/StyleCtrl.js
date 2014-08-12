c5972ddb49359938e8001333a49ada2f5de209ef
fredstales <f.sjolin@gmail.com>
2014-08-12 20:16:02 +0200

    Added Bootswatch cssfiles and code to change theme

--- public/admin/js/controllers/StyleCtrl.js
+++ public/admin/js/controllers/StyleCtrl.js
@@ -13,10 +13,25 @@ EasyPress.controller("StyleController", ['$scope', function($scope) {
 			$scope.elemProps[i] = typeof elem.style[i] == "number" ? elem.style[i].toString() : elem.style[i];
 		}
 	}
-	
+
 	$scope.setProperties = function() {
 		$scope.setMenu("main_menu");
 	};
-	
+
+	$scope.themes = {
+		'Amelia (Default)': 'amelia.css',
+		'Cerulean': 'cerulean.css',
+		'Cosmo': 'cosmo.css',
+		'Cyborg': 'cyborg.css',
+		'Darkly': 'darkly.css',
+		'Flatly': 'flatly.css',
+		'Journal': 'journal.css',
+		'Lumen': 'lumen.css',
+		'Readable': 'readable.css',
+		'Simplex': 'simplex.css',
+		'Slate': 'slate.css',
+		'Spacelab': 'spacelab.css',
+		'Superhero': 'superhero.css'
+		};
 
 }]);
